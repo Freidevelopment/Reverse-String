ba2bb2042c6c0071816c9241302a5b724f93bb67
Freidevelopment <163058281+Freidevelopment@users.noreply.github.com>
2024-04-11 11:37:11 -0400

    Initial commit

---
+++ js/site.js
@@ -0,0 +1,14 @@
+
+// Display the Message the user entered.
+function displayMessage() {
+    let inputBox = document.getElementById('message');
+    let message = inputBox.value;
+
+    Swal.fire({
+        title: "App Name",
+        text: message,
+        icon: "success",
+        backdrop: false
+      });
+
+}
